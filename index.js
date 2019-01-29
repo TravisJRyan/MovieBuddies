6f3cf8364825112da1f1a8a3409b4395d7d8ad86
Travis Ryan <travisryan@Traviss-MacBook-Pro.local>
2019-01-29 09:31:39 -0600

    removed semantic and switched to bootstrap

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express'); // Express module
-const app = express(); // init Express server
-app.set('view engine', 'pug'); // use Pug to render pages
+const express = require('express'); // Express module for creating a server to route pages
+const app = express(); // init Express server as a variable
+app.set('view engine', 'pug'); // have the server use Pug to render pages
 
-// Start server listening
+// Start server listening at port 3000 (localhost:3000)
 app.listen('3000', () => {
-    console.log('Server started on port 3000');
+    console.log('Server started on port 3000...');
 });
 
+// Allow the server to access static files in the same directory
 app.use(express.static(__dirname));
 
+// Route for the home page
 app.get('/', function(req, res){
-    res.render("test");
+    res.render("test"); // Render the pug file "test"
 });
