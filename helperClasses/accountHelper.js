47b40d4533eedfabda7ba5e7d6b16714e92900d1
Travis Ryan <tryan1996@gmail.com>
2019-05-03 11:10:16 -0500

    fixed disconnect issue

--- helperClasses/accountHelper.js
+++ helperClasses/accountHelper.js
@@ -7,7 +7,8 @@ var DB;
 
 // Function to Handle Disconnect Issue
 // Source: https://stackoverflow.com/questions/20210522/nodejs-mysql-error-connection-lost-the-server-closed-the-connection
-function handleDisconnect() {
+function handleDisconnect(callback) {
+    
     DB = mysql.createConnection({
         host: secretVars["host"],
         user: secretVars["user"],
@@ -18,236 +19,263 @@ function handleDisconnect() {
     DB.connect(function (err) {
         if (err) {
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000);
+            setTimeout(handleDisconnect(callback), 2000);
+        }
+        else {
+            callback();
         }
     });
     DB.on('error', function (err) {
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
+            handleDisconnect(callback);
         } else {
             throw err;
         }
     });
 }
 
-handleDisconnect();
-
 //TODO: function validates a login attempt
 module.exports.authenticate = function (email, password, callback) {
-    let existingUserSql = "SELECT * FROM users WHERE email = '" + email + "';";
-    let existingUserQuery = DB.query(existingUserSql, (err, results) => {
-        bcrypt.compare(password, results[0].pass, function (err, res) {
-            if (err) {
-                console.log(err);
-                callback([]);
-            } else {
-                if (res == false) { // no user, send to 404
-                    callback('404');
+    handleDisconnect(function () {
+        let existingUserSql = "SELECT * FROM users WHERE email = '" + email + "';";
+        let existingUserQuery = DB.query(existingUserSql, (err, results) => {
+            bcrypt.compare(password, results[0].pass, function (err, res) {
+                if (err) {
+                    console.log(err);
+                    callback([]);
                 } else {
-                    callback(results);
+                    if (res == false) { // no user, send to 404
+                        callback('404');
+                    } else {
+                        callback(results);
+                    }
                 }
-            }
+            });
         });
     });
 };
 
 // function to add a new friend
 module.exports.newFriendRequest = function (sender, receiver, callback) {
-    if (!sender || !receiver)
-        callback(-1);
-    else {
-        let newFriendSql = "INSERT INTO Friends VALUES('" + sender + "', '" + receiver + "', 0);";
-        let newFriendQuery = DB.query(newFriendSql, (err, results) => {
-            if (err) {
-                console.log(err);
-                callback(-1);
-            } else {
-                callback(results);
-            }
-        });
-    }
+    handleDisconnect(function () {
+        if (!sender || !receiver)
+            callback(-1);
+        else {
+            let newFriendSql = "INSERT INTO Friends VALUES('" + sender + "', '" + receiver + "', 0);";
+            let newFriendQuery = DB.query(newFriendSql, (err, results) => {
+                if (err) {
+                    console.log(err);
+                    callback(-1);
+                } else {
+                    callback(results);
+                }
+            });
+        }
+    });
 }
 
 // function processes a new account creation
 module.exports.createAccount = function (first, last, email, password, callback) {
-    // return false if null values
-    if (!first || !last || !email || !password) {
-        callback(false);
-    }
-
-    //insert query
-    let newUserSQL = "INSERT INTO users (email, pass, firstName, lastName) VALUES('" +
-        email + "','" + password + "','" + first + "','" + last + "');";
-
-    let newUserQuery = DB.query(newUserSQL, (err, results) => {
-        if (err) {
-            console.log(err);
+    handleDisconnect(function () {
+        // return false if null values
+        if (!first || !last || !email || !password) {
             callback(false);
-        } else {
-            callback(true);
         }
+
+        //insert query
+        let newUserSQL = "INSERT INTO users (email, pass, firstName, lastName) VALUES('" +
+            email + "','" + password + "','" + first + "','" + last + "');";
+
+        let newUserQuery = DB.query(newUserSQL, (err, results) => {
+            if (err) {
+                console.log(err);
+                callback(false);
+            } else {
+                callback(true);
+            }
+        });
     });
 }
 
 // get all of user's information for given email
 module.exports.getUser = function (email, callback) {
-    // return empty if no email
-    if (!email) {
-        callback({});
-    } else {
-        // Select query
-        let selectUserSQL = "SELECT firstName, lastName, age, gender, " +
-            "city, st, profileDescription, privacy FROM users WHERE email='" + email + "';";
-        let selectUserQuery = DB.query(selectUserSQL, (err, results) => {
-            if (err) {
-                console.log(err);
-                callback({});
-            } else {
-                if (results[0] == undefined) {
+    handleDisconnect(function () {
+        // return empty if no email
+        if (!email) {
+            callback({});
+        } else {
+            // Select query
+            let selectUserSQL = "SELECT firstName, lastName, age, gender, " +
+                "city, st, profileDescription, privacy FROM users WHERE email='" + email + "';";
+            let selectUserQuery = DB.query(selectUserSQL, (err, results) => {
+                if (err) {
+                    console.log(err);
                     callback({});
-                } else {  // return user information
-                    let user = {
-                        "email": email,
-                        "firstName": results[0].firstName,
-                        "lastName": results[0].lastName,
-                        "age": results[0].age,
-                        "gender": results[0].gender,
-                        "city": results[0].city,
-                        "st": results[0].st,
-                        "profileDescription": results[0].profileDescription,
-                        "privacy": results[0].privacy
-                    };
-                    callback(user);
+                } else {
+                    if (results[0] == undefined) {
+                        callback({});
+                    } else {  // return user information
+                        let user = {
+                            "email": email,
+                            "firstName": results[0].firstName,
+                            "lastName": results[0].lastName,
+                            "age": results[0].age,
+                            "gender": results[0].gender,
+                            "city": results[0].city,
+                            "st": results[0].st,
+                            "profileDescription": results[0].profileDescription,
+                            "privacy": results[0].privacy
+                        };
+                        callback(user);
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+    });
 }
 
 // update settings for a user
 module.exports.updateSettings = function (email, newSettings, callback) {
-    let updateUserSql = "UPDATE Users SET age=" + newSettings.age + ", gender='" + newSettings.gender +
-        "', city='" + newSettings.city + "', st='" + newSettings.state + "', profileDescription='" +
-        newSettings.profileDescription + "', privacy=" + newSettings.privacy + " WHERE email='" + email + "';";
-    let updateSettingsQuery = DB.query(updateUserSql, (err, results) => {
-        if (err)
-            console.log(err);
-        else
-            callback(results);
+    handleDisconnect(function () {
+        let updateUserSql = "UPDATE Users SET age=" + newSettings.age + ", gender='" + newSettings.gender +
+            "', city='" + newSettings.city + "', st='" + newSettings.state + "', profileDescription='" +
+            newSettings.profileDescription + "', privacy=" + newSettings.privacy + " WHERE email='" + email + "';";
+        let updateSettingsQuery = DB.query(updateUserSql, (err, results) => {
+            if (err)
+                console.log(err);
+            else
+                callback(results);
+        });
     });
 }
 
 module.exports.validateUserExists = function (email, callback) {
-    if (!email)
-        callback(false);
-    else {
-        let validateUserSql = "SELECT email FROM Users WHERE email = '" + email + "';";
-        let validateUserQuery = DB.query(validateUserSql, (err, results) => {
-            if (err) throw err;
-            if (results.length > 0)
-                callback(true);
-            else
-                callback(false);
-        });
-    }
-}
-
-module.exports.declineFriendship = function (senderEmail, receiverEmail, callback) {
-    let removeRequest = "DELETE FROM friends WHERE  sender = '" + senderEmail + "' and receiver = '" + receiverEmail + "';"
-    let removeQuery = DB.query(removeRequest, (err, results) => {
-        if (err) {
-            throw err;
+    handleDisconnect(function () {
+        if (!email)
             callback(false);
-        } else {
-            callback(true);
+        else {
+            let validateUserSql = "SELECT email FROM Users WHERE email = '" + email + "';";
+            let validateUserQuery = DB.query(validateUserSql, (err, results) => {
+                if (err) throw err;
+                if (results.length > 0)
+                    callback(true);
+                else
+                    callback(false);
+            });
         }
     });
 }
 
+module.exports.declineFriendship = function (senderEmail, receiverEmail, callback) {
+    handleDisconnect(function () {
+        let removeRequest = "DELETE FROM friends WHERE  sender = '" + senderEmail + "' and receiver = '" + receiverEmail + "';"
+        let removeQuery = DB.query(removeRequest, (err, results) => {
+            if (err) {
+                throw err;
+                callback(false);
+            } else {
+                callback(true);
+            }
+        });
+    });
+}
+
 module.exports.addFriendship = function (senderEmail, acceptingEmail, callback) {
-    let updateFriendStatus = "Update friends Set friendshipStatus = 1 WHERE receiver = '" + acceptingEmail + "';"
-    let updateFriendQuery = DB.query(updateFriendStatus, (err, results) => {
-        if (err) {
-            throw err;
-            callback(false);
-        } else {
-            callback(true);
-        }
+    handleDisconnect(function () {
+        let updateFriendStatus = "Update friends Set friendshipStatus = 1 WHERE receiver = '" + acceptingEmail + "';"
+        let updateFriendQuery = DB.query(updateFriendStatus, (err, results) => {
+            if (err) {
+                throw err;
+                callback(false);
+            } else {
+                callback(true);
+            }
+        });
     });
 }
 
 // get all friends for a given email
 module.exports.getFriends = function (email, callback) {
-    if (!email)
-        callback([]);
-    else {
-        let getFriendsSql = "SELECT sender, receiver FROM friends WHERE ((sender = '" + email + "') OR (receiver = '" + email + "')) " +
-            "AND friendshipStatus = 1;"
-        let getFriendsQuery = DB.query(getFriendsSql, (err, results) => {
-            if (err) throw err;
-            if (results.length == 0)
-                callback([]);
-            else {
-                callback(results);
-            }
-        });
-    }
+    handleDisconnect(function () {
+        if (!email)
+            callback([]);
+        else {
+            let getFriendsSql = "SELECT sender, receiver FROM friends WHERE ((sender = '" + email + "') OR (receiver = '" + email + "')) " +
+                "AND friendshipStatus = 1;"
+            let getFriendsQuery = DB.query(getFriendsSql, (err, results) => {
+                if (err) throw err;
+                if (results.length == 0)
+                    callback([]);
+                else {
+                    callback(results);
+                }
+            });
+        }
+    });
 }
 
 // check friend status (none/pending/accepted) for 2 given users
 module.exports.isFriend = function (firstEmail, secondEmail, callback) {
-    if (!firstEmail || !secondEmail)
-        callback(-1);
-    else {
-        let isFriendSql = "SELECT friendshipStatus FROM friends WHERE (sender = '" + firstEmail + "' AND receiver = '" + secondEmail + "') OR " +
-            "(sender = '" + secondEmail + "' AND receiver = '" + firstEmail + "')";
-        let isFriendQuery = DB.query(isFriendSql, (err, results) => {
-            if (err) throw err;
-            if (results.length == 0)
-                callback(-1); //callback -1 if no friend requests ever sent
-            else {
-                callback(results[0]['friendshipStatus']); //callback 0 or 1 based on friendship status (pending/accepted)
-            }
-        });
-    }
+    handleDisconnect(function () {
+        if (!firstEmail || !secondEmail)
+            callback(-1);
+        else {
+            let isFriendSql = "SELECT friendshipStatus FROM friends WHERE (sender = '" + firstEmail + "' AND receiver = '" + secondEmail + "') OR " +
+                "(sender = '" + secondEmail + "' AND receiver = '" + firstEmail + "')";
+            let isFriendQuery = DB.query(isFriendSql, (err, results) => {
+                if (err) throw err;
+                if (results.length == 0)
+                    callback(-1); //callback -1 if no friend requests ever sent
+                else {
+                    callback(results[0]['friendshipStatus']); //callback 0 or 1 based on friendship status (pending/accepted)
+                }
+            });
+        }
+    });
 }
 
 // Get all pending friend requests for a given user
 module.exports.getPendingRequests = function (email, callback) {
-    if (!email)
-        callback([]);
-    let selectPendingSql = "SELECT sender FROM Friends WHERE receiver = '" + email + "' AND friendshipStatus = 0;"
-    let selectPendingQuery = DB.query(selectPendingSql, (err, results) => {
-        if (err) throw err;
-        callback(results);
+    handleDisconnect(function () {
+        if (!email)
+            callback([]);
+        let selectPendingSql = "SELECT sender FROM Friends WHERE receiver = '" + email + "' AND friendshipStatus = 0;"
+        let selectPendingQuery = DB.query(selectPendingSql, (err, results) => {
+            if (err) throw err;
+            callback(results);
+        });
     });
 }
 
 // function is run after unit tests to clear test data
 module.exports.clearTestData = function (callback) {
-    let clearUserSql = "DELETE FROM Users WHERE email='qweqwe@uuop.com';";
-    let clearUserQuery = DB.query(clearUserSql, (err, results) => {
-        if(err)
-            console.log(err);
-        let clearRatingSql = "DELETE FROM Ratings WHERE email='test@test.com' AND movieID='tt5460858';";
-        let clearRatingQuery = DB.query(clearRatingSql, (err, results) => {
-            if(err)
+    handleDisconnect(function () {
+        let clearUserSql = "DELETE FROM Users WHERE email='qweqwe@uuop.com';";
+        let clearUserQuery = DB.query(clearUserSql, (err, results) => {
+            if (err)
                 console.log(err);
-            let clearFriendSql="DELETE FROM Friends WHERE sender='test@test.com' AND receiver='testing@test.com';"
-            let clearFriendQuery = DB.query(clearFriendSql, (err, results) => {
+            let clearRatingSql = "DELETE FROM Ratings WHERE email='test@test.com' AND movieID='tt5460858';";
+            let clearRatingQuery = DB.query(clearRatingSql, (err, results) => {
                 if (err)
                     console.log(err);
-                callback();
+                let clearFriendSql = "DELETE FROM Friends WHERE sender='test@test.com' AND receiver='testing@test.com';"
+                let clearFriendQuery = DB.query(clearFriendSql, (err, results) => {
+                    if (err)
+                        console.log(err);
+                    callback();
+                });
             });
         });
     });
 }
 
-module.exports.findUsers = function(searchTerm, callback) {
-    let findUsersSql = "SELECT email FROM Users WHERE email='"+searchTerm+"';";
-    let findUsersQuery = DB.query(findUsersSql, (err, results) => {
-        if (err)
-            console.log(err);
-        callback(results);
+module.exports.findUsers = function (searchTerm, callback) {
+    handleDisconnect(function () {
+        let findUsersSql = "SELECT email FROM Users WHERE email='" + searchTerm + "';";
+        let findUsersQuery = DB.query(findUsersSql, (err, results) => {
+            if (err)
+                console.log(err);
+            callback(results);
+        });
     });
 }
